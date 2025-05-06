d7a80b8444044de036d48962babd8ad7ffd1bb99
elibeber <beber.e@northeastern.edu>
2025-05-06 19:16:27 -0400

    Basic Movement

--- Worm Away/Assets/Scripts/Player.cs
+++ Worm Away/Assets/Scripts/Player.cs
@@ -1,17 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
-{
+{  
+    [SerializeField] private GameObject sprite;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
 
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>(); 
     }
 
     void Update()
+    {    
+        Movement();
+        FollowMouse();
+    }
+
+    private void Movement()
     {
+        rb.velocity = sprite.transform.up * speed;
+    }
+
+    private void FollowMouse()
+    {
+        // Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        float currentAngle = sprite.transform.eulerAngles.z;
+        float angle = Mathf.LerpAngle(currentAngle, targetAngle, rotationSpeed * Time.deltaTime);
         
+        sprite.transform.rotation = Quaternion.Euler(0, 0, angle);
+
     }
 }
