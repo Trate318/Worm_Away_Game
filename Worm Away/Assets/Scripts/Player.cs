0ed2930eddd746bb772d7567ade5a8e31c0b72df
Trate318 <ebeber318@gmail.com>
2025-05-09 21:42:45 -0400

    Started making movement Force-based

--- Worm Away/Assets/Scripts/Player.cs
+++ Worm Away/Assets/Scripts/Player.cs
@@ -11,38 +11,51 @@ public class Player : MonoBehaviour
     [SerializeField] private GameObject sprite;
     private Rigidbody2D rb;
     private Vector2 movement;
-    private float direction;
+    private Vector2 direction;
 
     [Header("Movement")]
-
     [SerializeField] private float speed;
+    [SerializeField] private float maxSpeedChange;
+    [SerializeField] private float accelSpeed;
+    [SerializeField] private float maxBoostSpeed;
     [SerializeField] private float boostSpeed;
     private bool isBoosting;
     [SerializeField] private float LerpRotSpeed;
     [SerializeField] private float stepRotSpeed;
     private float angle;
 
-    void Start()
-    {
+    void Start() {
         rb = gameObject.GetComponent<Rigidbody2D>(); 
     }
 
-    void Update()
-    {    
-        Movement();
+    void Update() {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        
         Rotation();
+
+        if (Input.GetKeyDown(KeyCode.G)) {
+            rb.AddForce(Vector2.down* 10);
+        }
+    }
+    void FixedUpdate() {
+        if (isBoosting) {
+            ;
+        }   
+        else {
+            Movement();
+        }
     }
 
-    private void Movement()
-    {
-        rb.velocity = sprite.transform.up * (isBoosting ? boostSpeed : speed);    
+    private void Movement() {
+        Vector2 targetSpeed = sprite.transform.up * speed ;
+        Vector2 speedDif = targetSpeed - rb.velocity;
+        rb.AddForce(movement);
     }
 
-    private void Rotation()
-    {
-        direction = Input.GetAxisRaw("Horizontal");   
+    private void Rotation() {
         float currentAngle = sprite.transform.eulerAngles.z;
-        float targetAngle = currentAngle - direction * stepRotSpeed;
+        float targetAngle = currentAngle - direction.x * stepRotSpeed;
         angle = Mathf.LerpAngle(currentAngle, targetAngle, LerpRotSpeed * Time.deltaTime);
         sprite.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
